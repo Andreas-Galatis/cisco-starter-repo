cea2d00ca64c1a116ce89210b1111e4da5ba22a6
Andreas Galatis <andreas>
2023-09-21 16:21:50 -0400

    Task 2 modal

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,15 +1,19 @@
-
+import React, { Component } from 'react';
 import './App.css';
+import Banner from './Banner';
+import Exhibit from './Exhibit';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Sextant</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Banner bannerText="Sextant" />
+        <Exhibit name="I'm an exhibit!"></Exhibit>
+        <Exhibit name="I'm also an exhibit"></Exhibit>
+        <Exhibit name="Don't forget aboout mr, I'm also an exhibit tool!"></Exhibit>
+      </div>
+   ); 
+  }
 }
-
-
-
 
 export default App;
