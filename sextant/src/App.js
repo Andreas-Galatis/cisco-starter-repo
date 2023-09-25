0747682b5127302cc578a7d90300c83e74908f16
Andreas Galatis <andreas>
2023-09-24 21:28:40 -0400

    Task 3

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import AddressDisplay from './IPAddress'
 import Banner from './Banner';
 import Exhibit from './Exhibit';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Banner bannerText="Sextant" />
-        <Exhibit name="I'm an exhibit!"></Exhibit>
-        <Exhibit name="I'm also an exhibit"></Exhibit>
-        <Exhibit name="Don't forget aboout mr, I'm also an exhibit tool!"></Exhibit>
-      </div>
+                <Banner bannerText="Sextant" />
+                <Exhibit name="Public IPv4 Address">
+                    <AddressDisplay url='https://api.ipify.org?format=json' />
+                </Exhibit>
+                <Exhibit name="Public IPv6 Address">
+                    <AddressDisplay url='https://api64.ipify.org?format=json' />
+                </Exhibit>
+            </div>
    ); 
   }
 }
---
+++ sextant/src/IPAddress.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const IPAddress = () => {
+    const [ipAddress, setIPAddress] = useState('')
+  
+    useEffect(() => {
+      fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => setIPAddress(data.ip))
+      .catch(error => console.log(error))
+    }, []);
+  
+    return (
+        ipAddress
+        
+      )
+    }
+
+
+  export default IPAddress;
